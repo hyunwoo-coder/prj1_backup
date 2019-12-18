89bb2e972a509bfa9e1773c3a6d78e500958f345
abc <abc@DESKTOP-TSSHPPO>
2019-12-18 23:19:47 +0900

    업무관리 관련 기능 구현할 컨트롤러 구현

---
+++ group4erp/src/com/group4/erp/controller/WorkController.java
@@ -0,0 +1,15 @@
+package com.group4.erp.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+//诀公包府 包访 扁瓷 备泅 能飘费矾
+
+@Controller
+public class WorkController {
+	
+	
+}
